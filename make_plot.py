0d74448ff423fdfb1f601cb79dcabd7f8374f845
YannFra <yfrab@hotmail.fr>
2019-11-10 11:57:02 -0800

    update upload_dataset

--- make_plot.py
+++ make_plot.py
@@ -4,10 +4,10 @@
 import geopandas as gpd
 
 db_bike_path="data/geo_export_0ff3043a-1833-476b-afe0-779dfa091ebd.shp"
-db_roads_path="data/tl_2017_06075_roads.shp"
-
 db_bike=gpd.read_file(db_bike_path)
-db_bike.symbology=db_bike.symbology.apply(lambda x: "BIKE LANE" if x=="Bike Lane" else x)
+
+
+db_roads_path="data/tl_2017_06075_roads.shp"
 db_roads=gpd.read_file(db_roads_path)
 
 
@@ -68,44 +68,6 @@ layout=row(SF,checkbox_choices)
 
 
 show(layout)
-
-# In[9]:               
-#updatingJS="""
-#            
-#    var x =source.data['total'];
-#    
-#    var coefs=[s_pop.value,s_solar.value,s_deforest.value,
-#        s_water.value,s_network.value,s_grid.value,s_politic.value];
-#    
-#    var max=0;
-#    
-#    for (var i = 0; i < x.length; i++) {
-#    
-#        x[i]=1;
-#        
-#        for (var j = 0; j<variablesNames.length; j++){
-#            x[i]=x[i]*source.data[variablesNames[j]][i]**coefs[j];
-#        }
-#            
-#        if (max < x[i]) {
-#            max=x[i];
-#        } 
-#    }
-#    
-#    for (var i = 0; i < x.length; i++) {
-#            x[i]=x[i]/max;
-#    }
-#    
-#    source.change.emit();
-#"""
-#
-#self.widgets[7].callback=CustomJS(args=updatingRequiredParameters,code=updatingJS)
-
-
-
-# In[9]:
-
-db_bike.symbology.value_counts()
 
 
 
--- upload_dataset.py
+++ upload_dataset.py
@@ -8,7 +8,8 @@ import os
 import zipfile
 from urllib.request import urlretrieve
 
-url_db="https://data.sfgov.org/api/geospatial/ygmz-vaxd?method=export&format=Shapefile"
+url_bike_db="https://data.sfgov.org/api/geospatial/ygmz-vaxd?method=export&format=Shapefile"
+url_street_db="https://catalog.data.gov/dataset/tiger-line-shapefile-2017-county-san-francisco-county-ca-all-roads-county-based-shapefile"
 
 #create the folder where we will save our dataset 
 data_dir="data"
@@ -16,17 +17,39 @@ try:os.mkdir(data_dir)
 except:pass
 
 
-#Downlaod the zip file available on the website if not already in `data`
-zip_name="bikeway_network.zip"
-zip_path=data_dir+"/"+zip_name
+#Downlaod the bike zip file available on the website if not already in `data`
+bike_zip_name="bikeway_network.zip"
+bike_zip_path=data_dir+"/"+bike_zip_name
                  
-if not os.path.exists(zip_path):
-    urlretrieve(url_db,zip_path)
-    print("Zip file containing the shapefile downloaded")
+if not os.path.exists(bike_zip_path):
+    
+    print("Downloading Bike Zip file")
+    urlretrieve(url_bike_db,bike_zip_path)
+    print("Download finished.")
+          
+    print("Starting unzipping")
+    with zipfile.ZipFile(bike_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(data_dir)
+    print("Bike Zip file unzipped")
+    
 else:
-    print("Zip file containing the shapefile already there")
+    print("Bike Network Db file already downloaded")
     
     
-#Unzip the zip file
-with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-    zip_ref.extractall(data_dir)
+#Downlaod the streets zip file available on the website if not already in `data`
+street_zip_name="streets_SF.zip"
+street_zip_path=data_dir+"/"+street_zip_name
+                 
+if not os.path.exists(street_zip_path):
+    
+    print("Downloading Street SF Zip file")
+    urlretrieve(url_street_db,street_zip_path)
+    print("Download finished.")
+          
+    print("Starting unzipping")
+    with zipfile.ZipFile(bike_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(data_dir)
+    print("Street SF Zip unzipped")
+    
+else:
+    print("Streets SF Db file already downloaded") 
